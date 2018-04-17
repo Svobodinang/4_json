ef6e8cac45f0731878c17a8da63a310ce33e17e1
svobodinang <svobodinang@mail.ru>
2018-04-17 22:51:01 +0300

    first commit

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,18 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding="utf-8", newline="") as f:
+        return json.load(f)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data,
+                     ensure_ascii=False,
+                     indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input("введите путь к файлу: ")
+    filepath
+    data = load_data(filepath)
+    pretty_print_json(data)
