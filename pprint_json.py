105c4408d5da2b6359897023e42de5bbf2385cb2
svobodinang <svobodinang@mail.ru>
2018-04-24 22:17:28 +0300

    add except for json file

--- pprint_json.py
+++ pprint_json.py
@@ -2,34 +2,35 @@ import json
 import sys
 import os
 
+
 def load_data(filepath):
     with open(filepath, encoding="utf-8") as json_file:
         return json.load(json_file)
 
 
-def pretty_print_json(data_unsorted):
-    print(json.dumps(data_unsorted,
-                     ensure_ascii=False,
-                     indent=4))
+def pretty_print_json(data_decoder):
+    print(json.dumps(
+        data_decoder,
+        ensure_ascii=False,
+        indent=4
+    ))
 
 
-def input_json_path():
-    file_json = sys.argv[1]
-
-    dirname, exname = os.path.splitext(file_json)
-
-    if os.path.isfile(file_json):
-        if exname == ".json":
-            return file_json
-        else:
-            print("Данный файл имеет расширение не .json")
-            return 0
-    else:
+def check_json_file():
+    try:
+        load_data(sys.argv[1])
+        return "ok"
+    except json.decoder.JSONDecodeError:
+        print("В файлене json текст")
+        return None
+    except IndexError:
+        print("Вы не указали путь к файлу")
+        return None
+    except FileNotFoundError:
         print("Такого файла не существует")
-        return 0
+        return None
 
 
 if __name__ == '__main__':
-    inputh_path = input_json_path()
-    if inputh_path!=0:
-        pretty_print_json(load_data(inputh_path))
+    if check_json_file() is not None:
+        pretty_print_json(load_data(sys.argv[1]))
